c2db2b983e33b4c50a54a0894d041c926b4781e2
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-12 19:57:02 -0400

    Add config.ts

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const apiURL =
+	"https://hn.algolia.com/api/v1/search_by_date?query=nodejs";
+
+export const postProperties = {
+	author: "author",
+	story_id: "id",
+	story_title: "story_title",
+	title: "title",
+	created_at: "createdAt",
+	_tags: "tags",
+};
