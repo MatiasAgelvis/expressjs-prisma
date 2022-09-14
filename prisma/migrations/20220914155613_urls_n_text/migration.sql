74b1bc51e95576d49d88837420ec802a75ebf6cc
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-14 12:25:05 -0400

    Update DB shcema
    Add url field
    Add text field

---
+++ prisma/migrations/20220914155613_urls_n_text/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `url` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "text" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "url" TEXT NOT NULL;
